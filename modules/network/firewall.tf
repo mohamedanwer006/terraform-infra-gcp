f10652bd4e60f739375ce8b845d362a006417088
mohamedanwer006 <mohamedanwer006@gmail.com>
2022-10-06 11:43:36 +0200

    Add firewall to network module

---
+++ modules/network/firewall.tf
@@ -0,0 +1,27 @@
+#------------------------------------------------------------------------------#
+#                                    Firewall                                  #
+#------------------------------------------------------------------------------#
+
+
+resource "google_compute_firewall" "fw_allow_ssh" {
+  name    = "vois-allow"
+  network = google_compute_network.vpc.name
+  # SSH
+ 
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  # HTTP/s
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+
+  # Allow traffic from google IAP 
+  source_ranges = ["35.235.240.0/20"] // allow only from the IAP
+
+  // No target_tags to apply to all instances 
+
+}
+
